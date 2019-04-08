ab6c7a33ef77118121f9d40e188664591c7bbfe8
Germano Costa Neto <germano.cneto@gmail.com>
2019-04-08 11:26:55 -0300

    Update TrialEvalut.R

--- R/TrialEvalut.R
+++ R/TrialEvalut.R
@@ -17,47 +17,39 @@
 #' @references 1. Colombari Filho JM, de Resende MDV, de Morais OP, de Castro AP, Guimarães ÉP, Pereira JA, et al. Upland rice breeding in Brazil: A simultaneous genotypic evaluation of stability, adaptability and grain yield. Euphytica. 2013;192(1):117–29.
 #' @references 2. Resende MDV, Duarte JB. Precisão e controle de qualidade em experimentos de avaliação de cultivares. Pesquisa Agropecuária Tropical. 2007;37(3):182–94.
 
-TE1<-function(df,y,rep, gen, trials, accuracy){
+TE1<-function(df,y,rep, gen, trials, accuracy,print=FALSE){
 
-  y <- df[,y]
-  g <- df[,gen]
-  r <- df[,rep]
-  e <- df[,trials]
-  dfa<-data.frame(y,g,r,e)
-
-
-  Ntrials <-length(unique(dfa$e))
-  results<-data.frame(matrix(nrow = Ntrials,ncol=10))
-
-  names(results)<-c("Trial","mean","Vg","Vres","Vf",
-                    "h2","rgg","CVg","CVe","Diagnostic")
-
-  for(i in 1:Ntrials){
-
-    cat("Running ",i, "Trial ", levels(dfa$e)[i], '\n')
-
-    # Subseting a target trial
-    A<-subset(dfa, e == levels(e)[i])
+  y <- df[, y]
+  g <- df[, gen]
+  r <- df[, rep]
+  e <- df[, trials]
+  dfa <- data.frame(y, g, r, e)
+  Ntrials <- length(unique(dfa$e))
+  results <- data.frame(matrix(nrow = Ntrials, ncol = 10))
+  names(results) <- c("Trial", "mean", "Vg", "Vres", "Vf", 
+                      "h2", "rgg", "CVg", "CVe", "Diagnostic")
+  for (i in 1:Ntrials) {
+    cat("Running ", i, "Trial ", levels(dfa$e)[i], "\n")
+    A <- subset(dfa, e == levels(e)[i])
     r <- length(unique(A$r))
-
-    # Linear mixed model equation
-    modelo<-lme4::lmer(y~1+r+(1|g),A)
-
-    # Outputs
-    results$Trial[i]<-levels(as.factor(A$e))[i]
-    results$Vg[i]<-round(lme4::VarCorr(modelo)[[1]][1],5)
-    results$Vres[i]<-round(lme4::sigma(modelo)^2,5)
-
-
-    results$Vf[i]<-(results$Vg[i]+results$Vres[i])
-    results$h2[i]<-round(results$Vg[i]/(results$Vg[i]+(results$Vres[i]/r)),4)
-    results$mean[i]<-fixef(modelo)[[1]]
-    results$CVg[i]<-round(100*(sqrt(results$Vg[i])/results$mean[i]),3)
-    results$CVe[i]<-round(100*(sqrt(results$Vf[i])/results$m[i]),3)
-    results$rgg[i]<-round(sqrt(1-(1/(1+r*(results$CVg[i]/results$CVe[i])^2))),3)
-    results$Diagnostic[i]<-ifelse(results$rgg[i] < accuracy,"Remove","-")
+    modelo <- lme4::lmer(y ~ 1 + r + (1 | g), A)
+    results$Trial[i] <- levels(as.factor(A$e))[i]
+    results$Vg[i] <- round(lme4::VarCorr(modelo)[[1]][1], 
+                           5)
+    results$Vres[i] <- round(sigma(modelo)^2, 5)
+    results$Vf[i] <- (results$Vg[i] + results$Vres[i])
+    results$h2[i] <- round(results$Vg[i]/(results$Vg[i] + 
+                                            (results$Vres[i]/r)), 4)
+    results$mean[i] <- fixef(modelo)[[1]]
+    results$CVg[i] <- round(100 * (sqrt(results$Vg[i])/results$mean[i]), 
+                            3)
+    results$CVe[i] <- round(100 * (sqrt(results$Vf[i])/results$m[i]), 
+                            3)
+    results$rgg[i] <- round(sqrt(1 - (1/(1 + r * (results$CVg[i]/results$CVe[i])^2))), 
+                            3)
+    results$Diagnostic[i] <- ifelse(results$rgg[i] < accuracy, 
+                                    "Remove", "-")
   }
-  print(knitr::kable(results))
+  if(isTRUE(print)){print(knitr::kable(results))}
   return(results)
-
 }
